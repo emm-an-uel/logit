eca63972fd9ceacd760f8d9a8ac3ba1aeca4509a
emmanuel <sunartio.emmanuel@gmail.com>
2022-08-05 22:07:02 +0930

    main log activity created

---
+++ app/src/main/java/com/example/homeworklogapp/MainLogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.homeworklogapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_log)
+    }
+}
