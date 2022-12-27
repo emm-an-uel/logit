3be79fe591b103b9ff2f5f53d73f0a2710d81ebb
emmanuel <sunartio.emmanuel@gmail.com>
2022-12-28 09:46:28 +1030

    before converting activity to fragment

--- app/src/main/java/com/example/logit/ParentActivity.kt
+++ app/src/main/java/com/example/logit/ParentActivity.kt
@@ -2,17 +2,23 @@ package com.example.logit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.logit.databinding.ActivityParentBinding
+import com.example.logit.mainlog.ViewModelMainLog
 
 private lateinit var appBarConfiguration: AppBarConfiguration
 private lateinit var binding: ActivityParentBinding
 
 class ParentActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ViewModelMainLog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,10 +26,24 @@ class ParentActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.appBarParent.toolbar)
 
+        // initialize viewModel
+        viewModel = ViewModelProvider(this)[ViewModelMainLog::class.java]
+
+        // initialize data
+        viewModel.apply {
+            initListCardColors()
+            initListSettings()
+            initTaskLists()
+            createConsolidatedListTodo()
+            createConsolidatedListDone()
+            initSubjectColor()
+        }
+
         // nav drawer
         val drawerLayout = binding.drawerLayout
         val navView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)!!
+        val navController = navHostFragment.findNavController()
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_log, R.id.nav_calendar
